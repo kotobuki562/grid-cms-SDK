0a2c9fcd19a27d6f1a52e0ff28873fc8a1882e94
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2021-12-03 20:48:13 +0900

    🐛

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
   roots: ["<rootDir>/test"],
 
